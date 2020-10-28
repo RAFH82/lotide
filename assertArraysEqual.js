4e33a048a4142c5cf82c9bf012abd49fe830864b
RAFH82 <hrechkar@gmail.com>
2020-10-28 01:41:03 +0000

    completed exercise

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -25,3 +25,4 @@ assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
 
+
