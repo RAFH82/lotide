7eb49815686fd0f8193ad477b6d6af23e2d1e995
RAFH82 <hrechkar@gmail.com>
2020-10-28 04:20:03 +0000

    completed exercise

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (array1, array2) => {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const flatten = (array) => {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length >= 1) {
+      for (let j = 0; j < array[i].length; j++) {
+        flattenedArray.push(array[i][j]);
+      }
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  console.log(flattenedArray);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
