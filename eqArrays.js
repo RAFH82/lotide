570cb6e4db81914b4aa9bfad5196c212bb5d5378
RAFH82 <hrechkar@gmail.com>
2020-10-28 01:02:43 +0000

    completed eqArrays exercise

--- eqArrays.js
+++ eqArrays.js
@@ -16,6 +16,7 @@ const eqArrays = (array1, array2) => {
   return true;
 };
 
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
