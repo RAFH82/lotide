fe4464282d73b35bfef7e887e5e4352c3fbaba5a
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-10-28 22:26:31 -0700

    added notes explaining the code flow

--- eqObjects.js
+++ eqObjects.js
@@ -20,15 +20,15 @@ const eqArrays = (array1, array2) => {
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
+    return false; // converts object into an array of keys and compares their lengths
   }
-  for (const key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
+  for (const key of Object.keys(object1)) { // iterates through each element (aka keys) of the object1 array 
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // for eack key, it checks to see if its value is an array in both objects
+      if (!eqArrays(object1[key], object2[key])) { // satisfied that they key's values are both arrays, it then passes them into the eqArrays function to compare
         return false;
       }
     } else {
-      if (object1[key] !== object2[key]) {
+      if (object1[key] !== object2[key]) { // compares the objects key values against each other
         return false;
       }
     }
