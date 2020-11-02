038b411aa3f9f6b5505ffd4db527370cf01c9ac5
RAFH82 <hrechkar@gmail.com>
2020-11-02 21:06:23 +0000

    removed requires=>assertEqual from tail.js and added to tailTest.js

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-
 const tail = (array) => {
   let newArray = [];
   for (let i = 1; i < array.length; i++) {
@@ -15,14 +6,4 @@ const tail = (array) => {
   return newArray;
 };
 
-let test = tail([1,2,3,4,5]);
-
-assertEqual(test.length, 4);
-assertEqual(test[0], 2);
-
-let test2 = tail([1]);
-let test3 = tail([]);
-
-console.log(test2);
-console.log(test3);
-
+module.exports = tail;
