b074f884bfa0c367f71859151869af3e2d8f584e
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-11-02 13:54:52 -0800

    added exports, cleaned up file

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  if (middle(actual)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = (array) => {
   if (array.length <= 2) return [];
   let middle = '';
@@ -30,11 +12,4 @@ const middle = (array) => {
   return(newArray);
 }
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-
-console.log(middle([1, 2, 3]))// => [2]
-assertArraysEqual([1, 2, 3, 4, 5], [3]) // => [3]
-
-console.log(middle([1, 2, 3, 4]))// => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
