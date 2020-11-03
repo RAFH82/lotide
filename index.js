6f20a574d6321b5ba17b0d0ce053e08d9432d7e1
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-11-02 18:03:55 -0800

    created index file

---
+++ index.js
@@ -0,0 +1,39 @@
+// all utility functions
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const without = require('./wihtout.js');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+// object containting exports
+module.exports = {
+
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  without: without,
+  takeUntil: takeUntil,
+  map: map,
+  flatten: flatten,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  assertObjectsEqual: assertObjectsEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+
+};
