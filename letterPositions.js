26d86f4f6de9a3d5083e7f1bd03512f741c06c90
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-11-02 19:14:58 -0800

    removed unecessary code, added module exports

--- letterPositions.js
+++ letterPositions.js
@@ -1,21 +1,3 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const letterPositions = (string) => {
   const results = {};
   for (const letter of string) {
@@ -30,5 +12,4 @@ const letterPositions = (string) => {
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").e, [1]);
-
+module.exports = letterPositions;
